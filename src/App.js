2eb4a1b93daacd895c106f69a12c7e17b415ba55
Kittimas Mankeswit <best@jenosize.co>
2021-09-05 13:19:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Route } from 'react-router-dom'
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [formList, setFormList] = useState({});
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then(response => {
+      setFormList(response.data)
+    });
+    fetch('https://api.github.com/users/kittimasmankeswit')
+      .then(res => res.json())
+      .then(data => {
+        console.log("datadatadata",data)
+      })
+  }, []);
+
+  console.log("formListformListformList", formList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        formList.forms?.map(item => {
+          console.log("itemitemitem", item)
+          return (
+            <div>
+              {item.name}
+            </div>
+          )
+        })
+      }
+      {
+        <img src={formList.sprites.back_default} />
+      } */}
     </div>
   );
 }
